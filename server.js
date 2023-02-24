20e3da056bf11a02fa28a7694ad1cfcbb982818b
Kurtmj93 <kurtmj93@gmail.com>
2023-02-24 13:34:46 -0500

    write models, connect sequelize

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express middleware for parsing JSON & html form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
