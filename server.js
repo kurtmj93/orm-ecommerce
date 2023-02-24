772b3647774756827ebfad84aeab98a8680da7f7
Kurtmj93 <kurtmj93@gmail.com>
2023-02-24 13:49:22 -0500

    add sequelize sync to server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+// import and initialize express
 const express = require('express');
-const routes = require('./routes');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
 // import sequelize connection
 const sequelize = require('./config/connection');
-
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 // express middleware for parsing JSON & html form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// import and use api routes
+const routes = require('./routes');
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
 });
